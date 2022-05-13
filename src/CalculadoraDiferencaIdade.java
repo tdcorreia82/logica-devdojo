f2a71d6c173cb9c18d37e0e19ac417566cc5f2fe
Thais Correia <thaiscorreia3841@gmail.com>
2022-05-13 17:38:42 -0300

    Initial setting for the project...and it is my first commit of my life

---
+++ src/CalculadoraDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadoraDiferencaIdade {
+    public static void main(String[] args) {
+        int firstAge = 30;
+        int secondAge = 15;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("The age difference is: " + ageDifference);
+    }
+}
