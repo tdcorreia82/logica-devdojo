f74c9cb0dabe29832dedeeb05d6043e571b53bba
Thais Correia <thaiscorreia3841@gmail.com>
2022-05-17 15:36:31 -0300

    if-else part 1

---
+++ src/CondicionaisAula1.java
@@ -0,0 +1,11 @@
+public class CondicionaisAula1 {
+    public static void main (String [] args) {
+        boolean condicao = false;
+        if (condicao) {
+            System.out.println ("Dentro do IF");
+        } else {
+            System.out.println ("Dentro do ELSE");
+        }
+        System.out.println ("Fora do IF");
+    }
+}
