ce776317124fbd79792e4543a8521f62ab5c3ad9
Thais Correia <thaiscorreia3841@gmail.com>
2022-05-17 16:51:54 -0300

    if-else part 2

---
+++ src/VerificaIdade.java
@@ -0,0 +1,13 @@
+public class VerificaIdade {
+    public static void main(String[] args) {
+        int idade = 25;
+        //==, !=, >, >=, <, <=
+        if (idade <= 18) {
+            System.out.println("Ainda não é Adulto");
+        } else {
+            System.out.println("Adulto");
+        }
+    }
+}
+
+
