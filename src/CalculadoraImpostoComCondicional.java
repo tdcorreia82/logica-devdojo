ec08cb97069eeda1a077fa9cd3fbb968accf2985
Thais Correia <thaiscorreia3841@gmail.com>
2022-05-18 15:11:28 -0300

    if-else part 3

---
+++ src/CalculadoraImpostoComCondicional.java
@@ -0,0 +1,15 @@
+public class CalculadoraImpostoComCondicional {
+    public static void main (String [] args) {
+        float salario = 4100.50F;
+        float resultado = 0F;
+        String porcentagem = "";
+        if (salario > 4500) {
+            resultado = salario * 0.3F;
+            porcentagem = "30%";
+        }else{
+            resultado = salario * 0.1F;
+            porcentagem = "10%";
+        }
+        System.out.println("O valor final em porcentagem de "+porcentagem+" é de "+resultado);
+    }
+}
