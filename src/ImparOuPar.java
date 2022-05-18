1c359a111b6ea88c4b8e015e138b75f2ecd673f6
Thais Correia <thaiscorreia3841@gmail.com>
2022-05-18 17:12:05 -0300

    if-else part 4

---
+++ src/ImparOuPar.java
@@ -0,0 +1,10 @@
+public class ImparOuPar {
+    public static void main(String[] args) {
+        int numero = 121212113;
+        if ((numero % 2) == 0) {
+            System.out.println("PAR: " + (numero%2));
+        }else{
+            System.out.println("IMPAR: " + (numero%2));
+        }
+    }
+}
